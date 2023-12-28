f320271d3e6263ce3c6ab31e00f7bc1981191294
jobnoone <cyril@dream.jobs>
2023-12-28 16:24:37 +0100

    Init commit

---
+++ src/main/kotlin/me/sahula/graalvmonaca/GraalvmOnAcaApplication.kt
@@ -0,0 +1,11 @@
+package me.sahula.graalvmonaca
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GraalvmOnAcaApplication
+
+fun main(args: Array<String>) {
+    runApplication<GraalvmOnAcaApplication>(*args)
+}
---
+++ src/test/kotlin/me/sahula/graalvmonaca/GraalvmOnAcaApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.sahula.graalvmonaca
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GraalvmOnAcaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
